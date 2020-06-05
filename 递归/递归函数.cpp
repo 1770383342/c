447b088361c510aa3c25a3812d3db5604b3519fb
1770383342 <1770383342@qq.com>
2020-06-05 14:55:32 +0800

    master

---
+++ 递归/递归函数.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int factorial(int n)
+{
+    if (n==0) return 1;
+    return n * factorial(n - 1);
+}
+int main(void)
+{
+    int a = 5;
+    
+    printf("%d\n",factorial(a));
+    
+    return 0;
+}
